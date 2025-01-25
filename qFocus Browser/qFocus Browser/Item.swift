4519f9fac85196fd76af219781a220176c1324b6
sascha <sascha@rhein.se>
2025-01-25 20:02:40 +0100

    Add .gitignore file

---
+++ qFocus Browser/qFocus Browser/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  qFocus Browser
+//
+//  Created by Sascha on 2025-01-25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
